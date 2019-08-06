097c249898f1423debb36dcd9e6ce7d993919fd4
Ally Preston <APreston@spartaglobal.com>
2019-08-06 08:39:56 +0100

    repaired issue with factor input

--- factorinput.py
+++ factorinput.py
@@ -1,7 +1,7 @@
 ## this script allows the user to run the function "factors" using prompted inputs
 
 # we need to import our function to run it
-from factorfunctionlegacy import *
+from factorfunction import *
 # we run a print statement to request the user to input a number
 
 print("please input the number to test")
